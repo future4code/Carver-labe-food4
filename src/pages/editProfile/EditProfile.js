24b9a0ffb461db532ff628072c09cc993b12bc0b
VICTOR FERREIRA DO VALE <victor.vale@uvvnet.com.br>
2022-01-10 14:27:23 -0300

    fazendo os inputs

--- src/pages/editProfile/EditProfile.js
+++ src/pages/editProfile/EditProfile.js
@@ -1,9 +1,53 @@
 import React from "react"
 
 const EditProfile = () => {
+    const [form, onChange, clear] = useForm({username:'', email: '', password: ''})
+
+    const onSubmitForm = (event) => {
+        event.preventDefault()
+        console.log(form)
+        requestSignup()
+    }
+
     return (
         <div>
-            EditProfile
+            <form onSubmit={onSubmitSignUp}>
+                        <input 
+                            required 
+                            value={form.name} 
+                            name={'name'} 
+                            onChange={onChangeInput} 
+                            placeholder='Nome e sobrenome'
+                            margin={'normal'}
+                            fullWidth
+                        />
+                        <input
+                            required 
+                            type='email' 
+                            value={form.email} 
+                            name={'email'} 
+                            onChange={onChangeInput} 
+                            placeholder='email@email.com'
+                            margin={'normal'}
+                            fullWidth
+                        />
+                        <input label="CPF"
+                            required 
+                            value={form.cpf} 
+                            name={'cpf'} 
+                            onChange={onChangeInput} 
+                            placeholder='000.000.000-0'
+                            margin={'normal'}
+                            fullWidth
+                        />
+                        <button
+                            type={'submit'}
+                            fullWidth
+                            margin={'normal'}
+                        >
+                        Salvar
+                        </button>
+                    </form>
         </div>
     )
 }
