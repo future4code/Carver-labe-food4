70d0056366f8237011f7174c4647130271864213
alisson <alissonabilio@gmail.com>
2022-01-11 16:03:38 -0300

    pagina de cadastro

--- src/pages/homePage/HomePage.js
+++ src/pages/homePage/HomePage.js
@@ -1,6 +1,12 @@
 import React from "react"
+import { useEffect } from "react"
+import {getRestaurants} from "../../api/API_Requests"
 
 const HomePage = () => {
+
+useEffect (() => {
+    getRestaurants()
+},[])
     return (
         <div>
             HomePage
--- src/pages/signupPage/SignupPage.js
+++ src/pages/signupPage/SignupPage.js
@@ -1,10 +1,73 @@
 import React from "react"
+import { FormContainer } from "./StyledSignup"
+import { useForm } from "../../hooks/useForm"
+import { ApiSignUp } from "../../api/API_Account"
+import { useHistory } from "react-router-dom"
+
+
 
 const SignupPage = () => {
+
+    const { form, onChange, clearInputs } = useForm({ name: "", email: "", cpf: "", password: "" })
+    const history = useHistory()
+
+
+
+    const trySubmit = (event) => {
+        event.preventDefault()
+        ApiSignUp(form, history)
+        clearInputs()
+    }
+
     return (
-        <div>
-            SignupPage
-        </div>
+        <FormContainer>
+            <form onSubmit={trySubmit}>
+                <input
+                    type="text"
+                    name="name"
+                    value={form.name}
+                    placeholder="digite o name"
+                    onChange={onChange}
+                    required
+                ></input>
+
+                <input
+                    type="email"
+                    name="email"
+                    value={form.email}
+                    placeholder="digite o email"
+                    onChange={onChange}
+                    required
+                ></input>
+
+                <input
+                    type="text"
+                    name="cpf"
+                    value={form.cpf}
+                    placeholder="digite o CPF"
+                    onChange={onChange}
+                    required
+                ></input>
+
+                <input
+                    type="password"
+                    name="password"
+                    value={form.password}
+                    placeholder="digite o senha"
+                    onChange={onChange}
+                    required
+                ></input>
+
+                {/* <input
+            type="password"
+            name="nome"
+            value={""}
+            placeholder="confirmar senha"
+        ></input> */}
+
+                <button type="submit">Cadastrar</button>
+            </form>
+        </FormContainer>
     )
 }
 export default SignupPage
