428674480c08de0d2dc6df85746d687c2e4a04de
ThiagoDaurizio <thiago_daurizio@hotmail.com>
2022-01-12 20:28:03 -0300

    detalhes style

--- src/pages/restaurantDetail/RestaurantDetail.js
+++ src/pages/restaurantDetail/RestaurantDetail.js
@@ -1,9 +1,13 @@
 import React, { useEffect, useState } from "react"
 import { useHistory, useParams } from "react-router-dom"
 import { getRestaurantDetails, getRestaurants } from "../../api/API_Requests"
+import { StyledDetails, StyledDetailsCard, StyledProducts, StyledProductsCard, StyledFoodArea } from "./StyledRestaurantDetail"
 
 const RestaurantDetail = () => {
     const [restaurantDetails, set_restaurantDetails] = useState([])
+
+    const styleButtonText = false
+    const numterButton = 3
 
     const history = useHistory()
     const params = useParams()
@@ -13,13 +17,67 @@ const RestaurantDetail = () => {
     },[])
 
     const details = restaurantDetails && restaurantDetails.map((item) => {
-        return <p>{item.name}</p>
+        return( <StyledDetailsCard>
+                <img src={item.logoUrl} alt={item.name} />
+                <p className="card-restaurant-title">{item.name.toUpperCase()}</p>
+                <p>{item.category}</p>
+                <div className="card-restaurant-money">
+                <p>{item.deliveryTime} - {item.deliveryTime + 10} min</p>
+                <p>Frete: R$ {item.shipping.toFixed(2)}</p>
+                </div>
+                <p>{item.address}</p>
+            </StyledDetailsCard>)
     })
 
     const products = restaurantDetails && restaurantDetails.map((item) => {
-        return <h6>{item && item.products.map((i, index)=> {
-            return index <= 2 && <p>{i.name}</p>
-        })}</h6>
+        return (<>
+            {item && item.products.map((i, index)=> {
+            return index <= 10 && 
+            (<StyledProductsCard>
+                <div>
+                    <img src={i.photoUrl} alt={i.name} />
+                </div>
+
+                <div className="card-content">
+                    <div className="card-content-top">
+                        <div className="card-content-texts">
+                            <p className="card-content-texts-title">{i.name}</p>
+                            <p className="card-content-texts-description">{i.description}</p>
+                        </div>
+
+                        <div className="card-content-marker">
+                                {
+                            numterButton
+                                ?
+                            <button>{numterButton}</button>
+                                :
+                            <div/>
+                                }
+
+                        </div>
+                    </div>
+
+                    <div className="card-content-bottom">
+                        <div>
+                            <p>R${i.price.toFixed(2)}</p>
+                        </div>
+
+                        <div className="card-content-cart">
+                            {/* TERNARIO DE ADICIONAR E REMOVER DO CARRINHO */}
+                                {
+                            styleButtonText 
+                                ? 
+                            <button className="card-button-add"> adicionar </button> 
+                                : 
+                            <button className="card-button-remove"> remover </button>
+                                }
+                        </div>
+                    </div>
+                </div>
+
+            </StyledProductsCard>)
+        })}</>) 
+
     })
 
 
@@ -27,15 +85,15 @@ const RestaurantDetail = () => {
     return (
         <div>
             <button onClick={() => console.log(restaurantDetails)}>INFO</button>
-            
+            <StyledDetails>
             {details}
+            </StyledDetails>
+
+                <StyledFoodArea>Comidas</StyledFoodArea>
+
+            <StyledProducts>
             {products}
-            {/* {restaurantDetails && restaurantDetails.map((item) => {
-                return <p>{item.name}</p>
-            })}
-
-            <p>{restaurant.name}</p> */}
-
+            </StyledProducts>
         </div>
     )
 }
