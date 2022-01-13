788b2d5240e3e4b3ddb7dbf298e9dd1677126612
VICTOR FERREIRA DO VALE <victor.vale@uvvnet.com.br>
2022-01-13 09:22:28 -0300

    filtro finalizado

--- src/pages/homePage/HomePage.js
+++ src/pages/homePage/HomePage.js
@@ -1,42 +1,56 @@
 import React, { useEffect, useContext, useState } from "react"
 import { useHistory, useParams } from "react-router-dom"
 import GlobalStateConstext from "../../global/GlobalStateContext"
-
 import { StyledHome } from "./StyledHome"
 import { goToDetail } from "../../routes/Coordinator"
+import Filter from "../../components/Filter"
 
 
 const HomePage = () => {
     const { states, setters } = useContext(GlobalStateConstext)
     const [restaurants, set_restaurants] = useState([])
+    const [name, setName] = useState('')
 
     const history = useHistory()
     const params = useParams()
 
 
-    useEffect(() => {
-        set_restaurants(states.restaurants.restaurants)
-    },[])
+    const handleName = (event) => {
+        setName(event.target.value)
+    }
+    
+    const rest = states.restaurants.restaurants
+    console.log('aqui é o rest:', rest)
+
+    const list = rest && rest.filter((x) => {
+        return x.name.toLowerCase().includes(name.toLowerCase())
+        
+
+    })
+
+
+
+    const filteredList = list && list.map((local) => {
+        return(
+            <div key={local.id} className="card" onClick={() => {goToDetail(history, local.id)}}>
+            <img src={local.logoUrl} />
+        
+            <div className="card-content">
+                <p className="card-upper">{local.name}</p>
+                <div className="card-bottom">
+                    <p>{local.deliveryTime}min</p> 
+                    <p>Frete: R$ {local.shipping.toFixed(2)}</p>
+                </div>
+            </div>
+        </div> 
+        )
+    })
 
     return (
         <StyledHome>
             HomePage
-            {/* <button onClick={() => console.log(states.restaurants.restaurants)}>INFO</button> */}
-            {states.restaurants.restaurants && states.restaurants.restaurants.map((local) => {
-                return(
-                    <div key={local.id} className="card" onClick={() => {goToDetail(history, local.id)}}>
-                        <img src={local.logoUrl} />
-                    
-                        <div className="card-content">
-                            <p className="card-upper">{local.name}</p>
-                            <div className="card-bottom">
-                                <p>{local.deliveryTime}min</p> 
-                                <p>Frete: R$ {local.shipping.toFixed(2)}</p>
-                            </div>
-                        </div>
-                    </div>
-                )
-            })}
+            <Filter name={name} handleName={handleName} />
+            {filteredList}
         </StyledHome>
     )
 }
